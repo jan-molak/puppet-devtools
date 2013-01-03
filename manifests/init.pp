91e437a4a3e955d1c828ffde0665d6d5b00b0ad5
Jan Molak <jan.molak@gmail.com>
2013-01-03 13:01:25 +0000

    You can provision the VM with IntelliJ and custom packages needed for development work and learn more about how to use the project from the README.md file. Lubuntu base box has been moved to dropbox so it can be easily downloaded as part of the 'vagrant up' procedure.

---
+++ manifests/init.pp
@@ -0,0 +1,66 @@
+# Class: java
+#
+# Parameters:
+#   The parameters listed here are not required in general and were
+#     added for use cases related to development environments.
+#   with_maven - whether or not install maven (default: false)
+#   with_ant   - whether or not install ant (default: false)
+# Actions:
+#
+# Requires:
+#   puppetlabs/apt
+#
+# Sample Usage:
+#  class { 'devtools':
+#    ide => 'IntelliJ',
+#    packages => [ 'cvs' ]
+#  }
+
+class devtools(
+  $ide = 'UNSET',
+  $packages = []
+) {
+  package { $packages: ensure => present; }
+
+  case $ide {
+    intellij: { install_intellij { '12.0.1': } }
+    eclipse: {
+      fail("Sorry, I haven't implemented eclipse support yet :( Stay tuned!")
+    }
+  }
+
+  define install_intellij() {
+    $downloadable = "ideaIU-${name}.tar.gz"
+    $temp_dir     = "/tmp"
+
+    download_file { $downloadable: site => "http://download.jetbrains.com/idea", cwd => $temp_dir }
+
+    exec { "Get IntelliJ dir name":
+      command => "tar -ztf ${downloadable} | grep -o -E '^[-A-Za-z0-9\\._]+' | uniq > ${temp_dir}/intellij_dir",
+      cwd     => $temp_dir,
+      creates => "${temp_dir}/intellij_dir",
+      require => Download_file[$downloadable]
+    }
+
+    exec { "Untar IntelliJ":
+      command => "tar xfz ${temp_dir}/${downloadable}",
+      cwd     => "/usr/local/lib",
+      require => Exec['Get IntelliJ dir name'],
+      unless  => "ls `cat ${temp_dir}/intellij_dir` 2>/dev/null"
+    }
+
+    exec { "ln -s /usr/local/lib/`cat ${temp_dir}/intellij_dir`/bin/idea.sh /usr/local/bin/idea":
+      require => Exec['Untar IntelliJ'],
+      creates => "/usr/local/bin/idea"
+    }
+  }
+
+  define download_file($site = '', $cwd = '/tmp', $user = 'root') {
+    exec { $name:
+      command => "wget ${site}/${name}",
+      cwd     => $cwd,
+      creates => "${cwd}/${name}",
+      user    => $user
+    }
+  }
+}
